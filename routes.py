e50e06e959b0fc5186cba45d05904d8b34681b39
Navaneeth Balaji <n.balaji@student.unsw.edu.au>
2018-04-23 00:09:28 +1000

    Added dashboard

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import *
 import requests
 import json
+import random
 
 app = Flask(__name__)
 
@@ -13,7 +14,22 @@ def main():
 
 @app.route("/home",methods=["GET", "POST"])
 def home():
-    response = requests.get("https://api.edamam.com/search?q=chicken&app_id=c565299e&app_key=\
-b90e6fb2878260b8f991bd4f9a8663ca&from=0&to=9")
-    print(response.json())
-    return render_template("home.html")
+
+    recipeLabels = []
+    recipeImageLinks = []
+    rand = random.randint(1,50)
+
+    response = requests.get("https://api.edamam.com/search?q=lol&app_id=c565299e&app_key=\
+b90e6fb2878260b8f991bd4f9a8663ca&from="+str(rand)+"&to="+str(rand+9))
+
+    jsonData = response.json()["hits"]
+
+
+    for item in jsonData:
+        recipeLabels.append(item.get('recipe').get('label'))
+        recipeImageLinks.append(item.get('recipe').get('image'))
+
+    print(recipeLabels)
+    print(recipeImageLinks)
+
+    return render_template("home.html", labelList = recipeLabels, imageList = recipeImageLinks)
