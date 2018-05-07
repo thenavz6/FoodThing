c48910cf93c2fa40fed0076263d3a217cb677669
SpidersInMyHair <mahbub.rishad@gmail.com>
2018-05-07 17:32:36 +1000

    Added Searchbar

--- routes.py
+++ routes.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 @app.route("/",methods=["GET", "POST"])
 def main():
     if request.method == "POST":
-        return redirect("/home")
-    else:
-        return render_template("login.html")
+        return redirect(url_for("dashboard"))
+    return render_template("login.html")
 
-@app.route("/home",methods=["GET", "POST"])
-def home():
+
+@app.route("/dashboard",methods=["GET", "POST"])
+def dashboard():
 
     recipeLabels = []
     recipeImageLinks = []
@@ -24,6 +24,33 @@ b90e6fb2878260b8f991bd4f9a8663ca&from="+str(rand)+"&to="+str(rand+9))
 
     jsonData = response.json()["hits"]
 
+    for item in jsonData:
+        recipeLabels.append(item.get('recipe').get('label'))
+        recipeImageLinks.append(item.get('recipe').get('image'))
+
+    print(recipeLabels)
+    print(recipeImageLinks)
+
+    if request.method == "POST":
+        print(request.form)
+        return redirect(url_for("searchRecipe", query = request.form["searchtext"]))
+
+    return render_template("dashboard.html", labelList = recipeLabels, imageList = recipeImageLinks)
+
+
+# Does essentially the same as the above. Except based on search query text
+# TODO Make the code recallable where its common with the above
+@app.route("/searchRecipe/<query>", methods=["GET", "POST"])
+def searchRecipe(query):
+
+    recipeLabels = []
+    recipeImageLinks = []
+    rand = random.randint(1,50)
+
+    response = requests.get("https://api.edamam.com/search?q="+str(query)+"&app_id=c565299e&app_key=\
+b90e6fb2878260b8f991bd4f9a8663ca&from="+str(rand)+"&to="+str(rand+9))
+
+    jsonData = response.json()["hits"]
 
     for item in jsonData:
         recipeLabels.append(item.get('recipe').get('label'))
@@ -32,4 +59,9 @@ b90e6fb2878260b8f991bd4f9a8663ca&from="+str(rand)+"&to="+str(rand+9))
     print(recipeLabels)
     print(recipeImageLinks)
 
-    return render_template("home.html", labelList = recipeLabels, imageList = recipeImageLinks)
+    if request.method == "POST":
+        print(request.form)
+        return redirect(url_for("searchRecipe", query = request.form["searchtext"]))
+
+    return render_template("dashboard.html", labelList = recipeLabels, imageList = recipeImageLinks)
+
