9929228e9e0628a4fd2cd6edebe1e7743c836c02
Nathan Ellis <z5160405@unsw.edu.au>
2018-05-08 15:38:08 +1000

    Finished Homepage

--- recipething-react/src/App.js
+++ recipething-react/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import HomeHeader from './components/Home_Header';
+import logo from './images/logo.png';
+import GoogleLogo from  './images/Google.png';
+import FacebookLogo from  './images/Facebook.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <HomeHeader />
+        <div className="homepage_body">
+            <div className="main_container">
+                <div className="logo_container">
+                    <img src={logo} alt="logo" className="logo"/>
+                </div>
+                <div className="centered">
+                    <h1>Welcome to RecipeThing!</h1>
+                    <h2>Please login using one of the following options:</h2>
+                </div>
+                <div className="button_block">
+                    <div className="auth_img_button">
+                        <img src={GoogleLogo} alt="GoogleLogo" className="auth_logo"/>
+                    </div>
+                    <div className="auth_img_button">
+                        <img src={FacebookLogo} alt="FacebookLogo" className="auth_logo"/>
+                    </div>
+                    <div className="auth_button">Continue as GUEST</div>
+                </div>
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
