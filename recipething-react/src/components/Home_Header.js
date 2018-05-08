7341bfbc927c1aee2959908917091365f3be018d
Nathan Ellis <z5160405@unsw.edu.au>
2018-05-08 15:48:51 +1000

    merged nathan to master working tree.

---
+++ recipething-react/src/components/Home_Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home_Header.css'
+
+export default class HomeHeader extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <div className="navbar-container">
+                    <div className="recipething">RECIPETHING</div>
+                </div>
+            </nav>
+        );
+    }
+}
