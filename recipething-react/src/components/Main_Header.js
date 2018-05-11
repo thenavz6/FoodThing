08c082335eee174cdddbce8babcd0cd8ac4d106e
Nathan Ellis <z5160405@unsw.edu.au>
2018-05-11 16:19:57 +1000

    Apparently the commit and push didn't work?

--- recipething-react/src/components/Main_Header.js
+++ recipething-react/src/components/Main_Header.js
@@ -8,7 +8,10 @@ const MainHeader = () => (
     <nav className="navbar navbar-default navbar-fixed-top">
         <div className="navbar-container">
           <img src={SearchImage} alt="searchimage" className="searchimage"/>
-          <div className="searchbox">Here goes the search box using react-search-box...</div>
+          <form method="POST">
+          	<input type="text" name="searchtext" placeholder="Search here..." className="searchbox"/>
+          	<input type="submit" value="Search" name="search_bt" className="searchbt"/>
+          </form>
           <div className="recipething">RECIPETHING</div>
           <NavLink to="/"><img src={NotificationsImage} alt="notifyimage" className="notifyimage"/></NavLink>
           <div className="rightbuttons">
