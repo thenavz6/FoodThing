d7919101ec64e7b7fc8b3864115c9327296f5736
Nathan Ellis <z5160405@unsw.edu.au>
2018-05-12 22:50:56 +1000

    Finalised favourites page.

--- recipething-react/src/components/Favourites.js
+++ recipething-react/src/components/Favourites.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MainHeader from './Main_Header';
 import Recipe from './Recipe';
+import OpenStar from '../images/open_star.png';
 
 class Favourites extends Component {
   render() {
@@ -8,10 +9,18 @@ class Favourites extends Component {
       <div>
         <MainHeader />
         <div className="main_container">
-          <div className="centered">
-            <h1>My Favourites</h1>
+          {/*This should only appear if a registered user has no favourites.*/}
+          <div className="loggedin">
+            <div className="centered">
+              <h2>Click (<img src={OpenStar} alt="OpenStar" height="40" width="40"/>) to add recipes to your favourites.</h2>
+            </div>
           </div>
+          <div className="notloggedin">
+            <div className="centered">
+              <h1>My Favourites</h1>
+            </div>
           <Recipe />
+          </div>
         </div>
       </div>
     );
